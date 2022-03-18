542d6ac65f3156fe07d5c66c7f88b8dbbd27bca2
Khayman King <khaymanak@gmail.com>
2022-03-18 16:36:29 -0700

    setup form

--- app.js
+++ app.js
@@ -1,2 +1,59 @@
-const form = document.getElementById('form');
+const form = document.querySelector('form');
 
+const optionButtonOne = document.querySelector('#option-one-vote');
+const optionButtonTwo = document.querySelector('#option-two-vote');
+const optionMinusButtonOne = document.querySelector('#option-one-minus');
+const optionMinusButtonTwo = document.querySelector('#option-two-minus');
+const pastpollsEl = document.querySelector('.past-poll-results');
+const endPoll = document.querySelector('#poll-end');
+const pollQuest = document.querySelector('.poll-question');
+const optOne = document.queryCommandValue('.option-one');
+const optTwo = document.queryCommandValue('.option-two');
+const optOneVotes = document.querySelector('#option-one-votes-result');
+const optTwoVotes = document.querySelector('#option-two-votes-result');
+
+
+//state 
+
+let oneVotes = 0;
+let twoVotes = 0;
+let question = '';
+let optionOne = '';
+let optionTwo = '';
+
+const pastArray = [];
+
+optionButtonOne.addEventListener('click', () => {
+    oneVotes++;
+
+    optOneVotes.textContent = oneVotes;
+});
+
+optionButtonTwo.addEventListener('click', () => {
+    twoVotes++;
+
+    optTwoVotes.textContent = twoVotes;
+});
+
+optionMinusButtonOne.addEventListener('click', () => {
+    oneVotes--;
+
+    optOneVotes.textContent = oneVotes;
+});
+
+optionMinusButtonTwo.addEventListener('click', () => {
+    oneVotes--;
+
+    optTwoVotes.textContent = twoVotes;
+});
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    quest = data.get('poll-question');
+    optOne = data.get('option-one');
+    optTwo = data.get('option-two');
+
+});
