c9290d40c97c50abafae123276673e8c43751953
Khayman King <khaymanaking@gmail.com>
2022-03-20 19:51:05 -0700

    almost done with app need to save options in render

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
-const form = document.querySelector('form');
+import { renderPoll } from './render.js';
 
+const form = document.getElementById('form');
 const optionButtonOne = document.querySelector('#option-one-vote');
 const optionButtonTwo = document.querySelector('#option-two-vote');
 const optionMinusButtonOne = document.querySelector('#option-one-minus');
 const optionMinusButtonTwo = document.querySelector('#option-two-minus');
-const pastpollsEl = document.querySelector('.past-poll-results');
-const endPoll = document.querySelector('#poll-end');
-const pollQuest = document.querySelector('.poll-question');
-const optOne = document.queryCommandValue('.option-one');
-const optTwo = document.queryCommandValue('.option-two');
-const optOneVotes = document.querySelector('#option-one-votes-result');
-const optTwoVotes = document.querySelector('#option-two-votes-result');
-
+const previousPollsEl = document.querySelector('.past-poll-results');
+const endPollButton = document.getElementById('poll-end');
+const pollQuest = document.getElementById('question-result');
+const optOne = document.getElementById('option-one-results');
+const optTwo = document.getElementById('option-two-results');
+const optOneVotes = document.getElementById('option-one-vote-results');
+const optTwoVotes = document.getElementById('option-two-vote-results');
+const startPollButton = document.getElementById('start-poll');
 
 //state 
 
@@ -22,6 +23,20 @@ let optionOne = '';
 let optionTwo = '';
 
 const pastArray = [];
+
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const formData = new FormData(form);
+    
+    question = formData.get('poll-question');
+    optionOne = formData.get('option-one');
+    optionTwo = formData.get('option-two');
+    
+    displayCurrentPoll();
+});
 
 optionButtonOne.addEventListener('click', () => {
     oneVotes++;
@@ -42,40 +57,63 @@ optionMinusButtonOne.addEventListener('click', () => {
 });
 
 optionMinusButtonTwo.addEventListener('click', () => {
-    oneVotes--;
+    twoVotes--;
 
     optTwoVotes.textContent = twoVotes;
 });
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    const data = new FormData(form);
-
-    quest = data.get('poll-question');
-    optOne = data.get('option-one');
-    optTwo = data.get('option-two');
-    
+startPollButton.addEventListener('click', () =>{
 
 });
 
 
-
-closePollButton.addEventListener('click', () => {
+endPollButton.addEventListener('click', () =>{
     form.reset();
 
-    const poll = formPoll();
+    const poll = createPoll();
 
-    previousPoll
-})
+    pastArray.push(poll);
+    resetState();
+    displayCurrentPoll();
+    displayList();
+});
 
-function formPoll() {
-    return{
-        quest: quest;
-        optOne: optOne; 
-        optTwo: optTwo;
-        oneVotes: oneVotes;
-        twoVotes: twoVotes;
+function displayCurrentPoll() {
+    pollQuest.textContent = question;
+    optOne.textContent = optionOne;
+    optTwo.textContent = optionTwo;
+    optOneVotes.textContent = oneVotes;
+    optTwoVotes.textContent = twoVotes; 
+}
+
+function displayList(){
+    previousPollsEl.textContent = '';
+
+    for (let pastPoll of pastArray){
+        const section = renderPoll(pastPoll);
+
+        previousPollsEl.append(section);
+    }
+}
+
+function createPoll() {
+    return {
+        question: question,
+        optOne: optOne,
+        optTwo: optTwo,
+        oneVotes: oneVotes,
+        twoVotes: twoVotes,
     };
 }
+
+function resetState(){
+    question = '';
+    optionOne = '';
+    optionTwo = '';
+    oneVotes = 0;
+    twoVotes = 0;
+
+}
+
+
 
--- render.js
+++ render.js
@@ -1,1 +1,19 @@
-ad
+export function renderPoll(pastPoll){
+    const section = document.createElement('section');
+    const h3 = document.createElement('h3');
+    const pTagOne = document.createElement('p');
+    const pTagTwo = document.createElement('p');
+    const optOneVotes = document.createElement('p');
+    const optTwoVotes = document.createElement('p');
+    
+    section.classList.add('past-results');
+    h3.textContent = pastPoll.question;
+    pTagOne.textContent = pastPoll.optionOne;
+    pTagTwo.textContent = pastPoll.optionTwo;
+    optOneVotes.textContent = pastPoll.oneVotes;
+    optTwoVotes.textContent = pastPoll.twoVotes;
+
+    section.append(h3, pTagOne, pTagTwo, optOneVotes, optTwoVotes);
+
+    return section;
+}
